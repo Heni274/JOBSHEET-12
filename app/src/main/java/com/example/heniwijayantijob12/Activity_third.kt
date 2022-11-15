a4a86559741993624edb971f45e7b5af12d65d77
KhamilaNur <nasywahanif@gmail.com>
2022-11-15 11:33:50 +0700

    first commit

---
+++ app/src/main/java/com/example/heniwijayantijob12/Activity_third.kt
@@ -0,0 +1,11 @@
+package com.example.heniwijayantijob12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_third : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third)
+    }
+}
